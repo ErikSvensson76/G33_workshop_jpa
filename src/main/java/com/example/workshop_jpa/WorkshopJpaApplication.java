4be8f1a7a7b327b644a8372480560abfedf098fb
Erik Svensson <eosvensson@gmail.com>
2021-03-10 10:41:14 +0100

    First commit

---
+++ src/main/java/com/example/workshop_jpa/WorkshopJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshop_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workshop_jpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
